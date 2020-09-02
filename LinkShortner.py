d79105a1f35ee6a98179d8b395f2f2efeff3caf5
Silvio Santoriello <silviosanto6605@gmail.com>
2020-09-02 12:29:53 +0200

    Version 1.0.1

--- LinkShortner.py
+++ LinkShortner.py
@@ -5,41 +5,43 @@ import sys
 
 baseurl = "https://tinyurl.com/create.php?source=indexpage&url="
 
+"""LinkShortner will automatically shorten links thanks to tinyurl.com. 
+    You can also make links with custom aliases."""
 
-class Shortner:
 
-    @dispatch(str)
-    def shorten(self, url):
-        source = requests.get(baseurl + url).text
-        soup = BeautifulSoup(source, features="html.parser")
-        try:
-            shortned_link = soup.find("a", {"id": "copy_div"})
-            return shortned_link["href"]
+@dispatch(str)
+def shorten(url):
+    source = requests.get(baseurl + url).text
+    soup = BeautifulSoup(source, features="html.parser")
+    try:
+        shortned_link = soup.find("a", {"id": "copy_div"})
+        return shortned_link["href"]
 
-        except TypeError:
-            print("URL invalid or alias not available")
-            exit(1)
+    except TypeError:
+        print("URL invalid or alias not available")
+        exit(1)
 
-    @dispatch(str, str)
-    def shorten(self, url, alias):
-        source = requests.get(baseurl + url + "&alias=" + alias).text
-        soup = BeautifulSoup(source, features="html.parser")
-        try:
-            shortned_link = soup.find("a", {"id": "copy_div"})
-            return shortned_link["href"]
 
-        except TypeError:
-            print("URL invalid / Alias not available")
-            exit(1)
+@dispatch(str, str)
+def shorten(url, alias):
+    source = requests.get(baseurl + url + "&alias=" + alias).text
+    soup = BeautifulSoup(source, features="html.parser")
+    try:
+        shortned_link = soup.find("a", {"id": "copy_div"})
+        return shortned_link["href"]
+
+    except TypeError:
+        print("URL invalid / Alias not available")
+        exit(1)
 
 
 if __name__ == '__main__':
     try:
-        print(Shortner.shorten(sys.argv[1], sys.argv[2]))
+        print(shorten(str(sys.argv[1]), str(sys.argv[2])))
 
     except IndexError:
         try:
-            print(Shortner.shorten((sys.argv[1])))
+            print(shorten((str(sys.argv[1]))))
         except IndexError:
             print("Not enough arguments specified... exiting")
             exit(1)
---
+++ setup.py
@@ -0,0 +1,17 @@
+﻿from setuptools import setup
+
+setup(
+    name='LinkShortner',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/silviosanto6605/LinkShortner',
+    license='MIT',
+    author='Silvio Santoriello',
+    author_email='silviosanto6605@gmail.com',
+    description='LinkShortner will automatically shorten links thanks to tinyurl.com.  You can also make links with custom aliases.',
+    install_requires=[
+        'requests',
+        "multipledispatch",
+        'beautifulsoup4'
+    ],
+)
